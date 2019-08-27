9b5cc70c556071722d5ffa4e5ce2fa679826c6f5
dai <a24arc.ad@gmail.com>
2019-08-27 19:36:05 +0900

    feat: macアドレス比較用の記述を追加

--- app/Http/Controllers/Api/ReceiveController.php
+++ app/Http/Controllers/Api/ReceiveController.php
@@ -5,24 +5,32 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Observed;
+use App\Models\User;
 use Carbon\Carbon;
 
 class ReceiveController extends Controller
 {
 
+    private $now;
     private $observed;
+    private $user;
 
-    public function __construct(Observed $observed)
+    public function __construct(Observed $observed, User $user)
     {
+        $this->now = new Carbon('now');
         $this->observed = $observed;
+        $this->user = $user;
     }
 
     public function receive(Request $request)
     {
-        $now = new Carbon('now');
-        $addressArr = $this->makeAddressArr($request->all(), $now);
+        $macs = $request->all();
+        $users = $this->user->pluck('mac_address')->toArray();
 
+        $addressArr = $this->makeAddressArr($macs, $now);
         $this->observed->insert($addressArr);
+
+        dd(array_intersect($macs, $users));
 
         return $addressArr;
     }
